8a952ae11fd69a4cc5de09b3138f732d66e03479
Vladislav Raiuk <vladislav.raiuk@gmail.com>
2026-03-29 23:59:15 +0300

    fix(bootstrap): tsconfig references, page title, typed hooks pattern

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from './store'
 
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector = <T>(selector: (state: RootState) => T) => useSelector(selector)
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
